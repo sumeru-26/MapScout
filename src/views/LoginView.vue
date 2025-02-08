a63c9b135564c84a4a9f695efd7771ad47a0a09f
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2025-02-08 10:52:47 -0800

    initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+    import { ref } from 'vue'
+
+    import { Button } from '@/components/ui/button'
+    import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+    import { Input } from '@/components/ui/input'
+    import { Label } from '@/components/ui/label'
+    import { Separator } from '@/components/ui/separator'
+
+    import { ScanQrCode } from 'lucide-vue-next'
+
+    const code = ref('')
+
+    function submit() {
+        const [key, event, type] = decoded.split(' ')
+        const test = fetch("https://api.scoutalliance.live/", {
+            headers: {
+                "X-SA-Auth-Key": key
+            }
+        })
+    }
+
+</script>
+
+<template>
+    <div class="flex flex-row min-h-screen justify-center items-center">
+        <Card class="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle class="text-2xl">
+                    Login
+                </CardTitle>
+                <CardDescription>
+                    Enter the event code for your team to begin scouting.
+                </CardDescription>
+            </CardHeader>
+            <CardContent class="grid gap-4">
+                <div class="grid gap-2">
+                    <Label for="email">Code</Label>
+                    <Input id="code" type="text" />
+                </div>
+                <Separator label="or" />
+                <Button disabled>
+                    Scan QR code <ScanQrCode class="w-4 h-4 mr-2" />
+                </Button>
+            </CardContent>
+            <CardFooter>
+                <Button class="w-full" @click.stop.prevent="submit()">
+                    Sign in
+                </Button>
+            </CardFooter>
+        </Card>
+    </div>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <p>test</p>
+</template>
